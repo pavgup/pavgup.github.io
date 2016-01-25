9b380767e3e78de706c939bcc1b7f2506642154d
Pavan Gupta <pg8p@virginia.edu>
2016-01-25 03:34:35 -0500

    swapped in ng-repeat for contacts and moved to a controller as implementation to avoid scope whackitude

--- app/scripts/header/headerModule.js
+++ app/scripts/header/headerModule.js
@@ -8,5 +8,29 @@ angular.module('headerModule', [])
   console.log('headerModule run callback fired');
 }])
 .controller('HeaderCtrl', ['$scope', function($scope) {
-  $scope.name = 'Pavan Gupta';
+  console.log($scope);
+  this.myName = 'Pavan Gupta';
+  this.contacts = [
+    {
+      href: 'https://twitter.com/pavgup',
+      icon: 'fa-twitter',
+      serviceName: 'Twitter'
+    }, {
+      href: 'https://facebook.com/pavangupta',
+      icon: 'fa-facebook',
+      serviceName: 'Facebook'
+    }, {
+      href: 'https://github.compavgup',
+      icon: 'fa-github',
+      serviceName: 'Github'
+    }, {
+      href: 'https://linkedin.com/in/pavgup',
+      icon: 'fa-linkedin',
+      serviceName: 'LinkedIn'
+    }, {
+      href: 'mailto:pg8p@virginia.edu',
+      icon: 'fa-envelope-o',
+      serviceName: 'Email'
+    }
+  ];
 }]);
