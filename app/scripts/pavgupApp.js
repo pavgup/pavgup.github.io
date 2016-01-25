12ca26ec2edc6d7717dab73a2bacddac715ee4b3
Pavan Gupta <pg8p@virginia.edu>
2016-01-25 07:18:20 -0500

    Update 2016-01-25T12:18:13.637Z

--- app/scripts/pavgupApp.js
+++
@@ -1,8 +0,0 @@
-/* global angular */
-angular.module('pavgupApp', ['headerModule'])
-  .config([function() {
-    console.log('configure block parsed');
-  }])
-  .run([function() {
-    console.log('run blocak parsed');
-  }]);
